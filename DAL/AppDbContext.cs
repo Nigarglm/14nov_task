eaf5174433a08d95404ef3683adcf06206b9e91b
Nigarglm <tu7mk3dbf@code.edu.az>
2023-11-20 04:47:33 +0400

    added new task

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _14nov_task.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _14nov_task.DAL
+{
+    internal class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=DESKTOP-NK9HMU9\\MSSQLSERVER01;database=14NovTask;trusted_connection=true;integrated security=true");
+
+        }
+
+    }
+}
---
+++ Models/StudentService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _14nov_task.DAL;
+
+namespace _14nov_task.Models
+{
+    internal class StudentService
+    {
+        private static readonly AppDbContext _context = new AppDbContext();
+
+        public List<Student> GetAll()
+        {
+            return _context.Students.ToList();
+        }
+
+        public Student GetById(int id)
+        {
+            return _context.Students.FirstOrDefault(s => s.Id == id);
+            // _context.Students.Find(id); 
+        }
+
+        public void Create(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            Student studentToDelete = GetById(id);
+
+            if (studentToDelete != null)
+            {
+                _context.Students.Remove(studentToDelete);
+                _context.SaveChanges();
+            }
+        }
+    }
+
+}
